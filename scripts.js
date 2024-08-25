2e3af0dbf861ab6d54a24a5608994260d78f0a1d
Alan81887412 <kids81887412@gmail.com>
2024-08-26 04:03:56 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -18,40 +18,4 @@ iconClose.addEventListener('click',() => {
     wrapper.classList.remove('active-popup');
 });
 
-const audio = document.getElementById('audio-player');
-const seekSlider = document.getElementById('seek-slider');
-const currentTimeElem = document.getElementById('current-time');
-const durationElem = document.getElementById('duration');
 
-// 更新進度條和時間
-audio.addEventListener('timeupdate', () => {
-    const currentTime = audio.currentTime;
-    const duration = audio.duration;
-    seekSlider.value = (currentTime / duration) * 100;
-    currentTimeElem.textContent = formatTime(currentTime);
-});
-
-// 進度條變更
-seekSlider.addEventListener('input', () => {
-    const duration = audio.duration;
-    audio.currentTime = (seekSlider.value / 100) * duration;
-});
-
-// 設置音樂時長
-audio.addEventListener('loadeddata', () => {
-    durationElem.textContent = formatTime(audio.duration);
-});
-
-// 格式化時間
-function formatTime(seconds) {
-    
-    const minutes = Math.floor(seconds / 60);
-    const secs = Math.floor(seconds % 60);
-    return `${minutes}:${secs < 10 ? '0' : ''}${secs}`;
-}
-
-window.addEventListener('load', () => {
-    audio.play().catch(error => {
-        console.log('自動播放失敗:', error);
-    });
-});
