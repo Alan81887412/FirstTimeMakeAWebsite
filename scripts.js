a1973b72ec1f912ee47267099932ab1c99f55e7c
Alan81887412 <kids81887412@gmail.com>
2024-08-26 03:29:06 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -44,7 +44,14 @@ audio.addEventListener('loadeddata', () => {
 
 // 格式化時間
 function formatTime(seconds) {
+    
     const minutes = Math.floor(seconds / 60);
     const secs = Math.floor(seconds % 60);
     return `${minutes}:${secs < 10 ? '0' : ''}${secs}`;
 }
+
+window.addEventListener('load', () => {
+    audio.play().catch(error => {
+        console.log('自動播放失敗:', error);
+    });
+});
